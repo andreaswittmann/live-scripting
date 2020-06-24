94fb11721e7d204a2a913f12ad816a6a019242b2
lubuntu <you@example.com>
2020-06-24 19:41:15 +0200

    orgweb v1, overspanning web.

--- publish-project.el
+++ publish-project.el
@@ -6,8 +6,8 @@
       '(
 
        ;; ... add all the components here (see below)...
-       ("org" :components ("org-notes" "org-static"))
-
+        ("org" :components ("org-notes" "org-static" "org-themes" "themes"))
+        ("themes" :components ("org-themes-notes" "org-themes-static"))
 
 
        ("org-notes"
@@ -41,5 +41,68 @@
         :recursive t
         :publishing-function org-publish-attachment
         )
-      ))
+
+
+       ("org-themes-notes"
+        :base-directory "~/org/aw-org-html-themes/"
+        :base-extension "org"
+        :publishing-directory "/var/www/html/live-scripting/themes/"
+        ;;:exclude ".*"
+        ;;:include ["foobar.org"]
+        :recursive t
+        :publishing-function org-html-publish-to-html
+        :headline-levels 4             ; Just the default for this project.
+        :auto-preamble t
+        :auto-sitemap t                ; Generate sitemap.org automagically...
+        :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
+        :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
+        )
+
+       ("org-themes-static"
+        :base-directory "~/org/aw-org-html-themes/"
+        :base-extension "css\\|js\\|png\\|jpg\\|jpeg\\|gif\\|pdf\\|txt\\|mp3\\|ogg\\|swf"
+        :exclude ".git\\|LICENSE"
+        :publishing-directory "/var/www/html/live-scripting/themes/"
+        :recursive t
+        :publishing-function org-publish-attachment
+        )
+;; Ein gemeinsames Web für alle Projekte
+
+       ("orgweb" :components ("orgweb-notes" "orgweb-static" "orgweb-themes"))
+
+
+       ("orgweb-notes"
+        :base-directory "~/org/"
+        :base-extension "org"
+        :publishing-directory "/var/www/html/orgweb"
+        ;;:exclude ".*"
+        ;;:include ["foobar.org"]
+        :recursive t
+        :publishing-function org-html-publish-to-html
+        :headline-levels 4             ; Just the default for this project.
+        :auto-preamble t
+        :auto-sitemap t                ; Generate sitemap.org automagically...
+        :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
+        :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
+        )
+
+       ("orgweb-static"
+        :base-directory "~/org/"
+        :base-extension "css\\|js\\|png\\|jpg\\|jpeg\\|gif\\|pdf\\|txt\\|mp3\\|ogg\\|swf"
+        :exclude ".git\\|LICENSE"
+        :publishing-directory "/var/www/html/orgweb/"
+        :recursive t
+        :publishing-function org-publish-attachment
+        )
+
+       ("orgweb-themes"
+        :base-directory "~/org/aw-org-html-themes/styles"
+        :base-extension "css\\|js\\|png\\|jpg\\|jpeg\\|gif\\|pdf\\|txt\\|mp3\\|ogg\\|swf"
+        :exclude ".git\\|LICENSE"
+        :publishing-directory "/var/www/html/orgweb/styles"
+        :recursive t
+        :publishing-function org-publish-attachment
+        )
+
+       ))
 
